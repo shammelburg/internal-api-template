be8a7c9a9737a13dd6b60e0d3f8cc7cc45902772
Sander Hammelburg <sander@hammelburg.me>
2025-08-12 08:07:30 +0100

    backup

--- Internal.API/Models/QueryParams/SharedDriveQueryParams.cs
+++
@@ -1,6 +0,0 @@
-namespace Distrupol.Bank.API.Models.QueryParams;
-
-public class SharedDriveQueryParams
-{
-    public string SharedDrive { get; set; }
-}
--- Internal.API/Program.cs
+++ Internal.API/Program.cs
@@ -1,6 +1,5 @@
 using System.Security.Claims;
 using Distrupol.Bank.API.Extensions;
-using Distrupol.Bank.API.Models.QueryParams;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,19 +13,16 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
         cp => cp
-            .WithOrigins(builder.Configuration["ClientAddress"]) // Remove for Add-in
-            // .AllowAnyOrigin()  // Remove for Add-in
+            .WithOrigins(builder.Configuration["ClientAddress"])
+            // .AllowAnyOrigin() 
             .AllowAnyMethod()
             .AllowAnyHeader()
-            .AllowCredentials()); // Remove for Add-in
+            .AllowCredentials());
 });
 
 builder.Services.AddAuthentication("Windows");
 
 var app = builder.Build();
-
-// Force FileWatcherService to initialize
-// app.Services.GetRequiredService<FileWatcherService>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -35,11 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
-
-// Map the SignalR hub endpoint
-// app.MapHub<FolderHub>("/folderhub");
 
 var endpoints = app
     .MapGroup("/api")
@@ -53,76 +45,8 @@ if (bool.Parse(app.Configuration["UseWindowsAuthentication"]))
 endpoints.MapGet("/common",
     (ClaimsPrincipal user) => TypedResults.Ok(new
     {
-        username = user.GetDomainName(),
-        sharedDrives = builder.Configuration.GetSection("SharedDrives").Get<string[]>(),
-        DestinationDrive = builder.Configuration["DestinationDrive"]
+        username = user.GetDomainName()
     }));
 
-endpoints.MapPost("/shared-drive",
-    (SharedDriveQueryParams qp, ClaimsPrincipal user) =>
-    {
-        if (!Directory.Exists(qp.SharedDrive))
-        {
-            return Results.BadRequest("The directory path does not exist");
-        }
-
-        var files = Directory.GetFiles(qp.SharedDrive).Where(f =>
-        {
-            var fileName = Path.GetFileNameWithoutExtension(f);
-            return !fileName.StartsWith($".") && !string.IsNullOrEmpty(fileName);
-        });
-
-        var filesWithInfo =
-            files.Select(file => new { file = Path.GetFileName(file), info = File.GetCreationTime(file) });
-
-
-        return TypedResults.Ok(new { files = filesWithInfo });
-    });
-
-endpoints.MapPost("/destination-drive",
-    (DestinationDriveQueryParams qp, ClaimsPrincipal user) =>
-    {
-        var year = DateTime.Today.Year.ToString();
-        var month = DateTime.Today.Month.ToString().PadLeft(2, '0');
-        var destination = Path.Combine(qp.DestinationDrive, year, month);
-
-        if (!Directory.Exists(destination))
-        {
-            Directory.CreateDirectory(destination);
-        }
-
-        var files = Directory.GetFiles(qp.SharedDrive).Where(f =>
-        {
-            var fileName = Path.GetFileNameWithoutExtension(f);
-            return !fileName.StartsWith($".") && !string.IsNullOrEmpty(fileName);
-        });
-
-        if (qp.Uppercase)
-        {
-            qp.BatchNumber = qp.BatchNumber.ToUpper();
-        }
-
-        var fileCount = 0;
-        var list = new List<string>();
-        foreach (var file in files)
-        {
-            fileCount = fileCount + 1;
-
-            var fileName = Path.GetFileNameWithoutExtension(file);
-            var fileNameBatchNumber = qp.BatchNumber + $"-{fileCount}";
-
-            if (files.Count() == 1)
-            {
-                fileNameBatchNumber = qp.BatchNumber;
-            }
-
-            var dest = file.Replace(qp.SharedDrive, destination).Replace(fileName, fileNameBatchNumber);
-
-            File.Move(file, dest);
-            list.Add(dest.Replace(qp.DestinationDrive, ""));
-        }
-
-        return TypedResults.Ok(new { list = list });
-    });
 
 app.Run();
